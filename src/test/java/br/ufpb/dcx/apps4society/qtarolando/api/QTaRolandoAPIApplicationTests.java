d805386aa07e521cb8fe7c1b15b63e0fddf370be
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-09-01 16:27:12 -0300

    Renaming directories

--- src/test/java/br/ufpb/dcx/apps4society/qtarolando/api/QTaRolandoAPIApplicationTests.java
+++ src/test/java/br/ufpb/dcx/apps4society/qtarolando/api/QTaRolandoAPIApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.qtarolando.api;
+package br.ufpb.dcx.apps4society.qtarolando.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QtarolandoV1ApplicationTests {
+class QTaRolandoAPIApplicationTests {
 
 	@Test
 	void contextLoads() {
