71bb6a1bd1a6ecfdd7847c7bf1ee28a1a69dca9e
Ronellyson <ronellysonjulio@gmail.com>
2021-11-30 18:29:53 -0300

    #userManagement - Updating security configuration class

--- src/main/java/br/ufpb/dcx/apps4society/qtarolando/api/security/WebSecurityConfig.java
+++ src/main/java/br/ufpb/dcx/apps4society/qtarolando/api/security/WebSecurityConfig.java
@@ -6,14 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,46 +19,36 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserAccountService userDetailsService;
 
     @Autowired
     private Environment env;
+
+    private static final String[] PUBLIC_MATCHERS = {
+        "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/api/events",
+            "/api/events/{id}"
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
-            http.cors().and().csrf().disable();
-            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-            http.authorizeRequests().anyRequest().permitAll();
-        } else {
-            http.csrf().disable().authorizeRequests()
-                    .antMatchers(HttpMethod.GET, "/api/eventos").permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/eventos/{id}").permitAll()
-                    .anyRequest().authenticated()
-                    .and().formLogin().permitAll()
-                    .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
         }
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/materialize/**", "/style/**");
+        http.cors().and().csrf().disable();
+        http.authorizeRequests()
+            .antMatchers(PUBLIC_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+            .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
     }
 }
