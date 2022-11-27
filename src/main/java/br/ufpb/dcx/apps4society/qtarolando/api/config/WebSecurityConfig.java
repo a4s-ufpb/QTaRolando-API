205680f927a1eeaf32abbd502768348dcb52667a
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2022-11-27 12:21:10 -0300

    Configurando o SpringSecurity para permitir que as anotações de segurança sejam habilitadas

--- src/main/java/br/ufpb/dcx/apps4society/qtarolando/api/config/WebSecurityConfig.java
+++ src/main/java/br/ufpb/dcx/apps4society/qtarolando/api/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ import br.ufpb.dcx.apps4society.qtarolando.api.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
